350c46d736926f5b78f3921cdbb00d890277931d
kamandla jagan <67571869+jagan9@users.noreply.github.com>
2020-07-24 13:09:04 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+allquestions=[
+  {
+    question:"what is the color of water??",
+    options:["green","blue","red","orange"],
+    correct:1
+  },
+  {
+    question:"what is the color of blood??",
+    options:["black","white","grey","red"],
+    correct:3
+  },
+  {
+    question:"what is the color of milk??",
+    options:["black","white","grey","red"],
+    correct:1
+  },
+  {
+    question:"what is the color of rose??",
+    options:["black","white","grey","red"],
+    correct:3
+  },
+  {
+    question:"what is the color of hair??",
+    options:["black","white","grey","red"],
+    correct:0
+  },
+  {
+    question: "Who invented JavaScript?",
+    options: ["Douglas Crockford","Sheryl Sandberg","Brendan Eich","none of above" ],
+    correct: 2
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    options: ["Node.js","TypeScript","npm", "none of above"
+    ],
+    correct:2
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    options: ["Angular","jQuery","RequireJS","ESLint"],
+    correct:3
+  },
+  {
+   question: "Who is Peter Parker’s nemesis in the first Spider-Man film?",
+   options:["The Joker", "Magneto", "The Green Goblin","None"],
+   correct:3
+  },
+  {
+   question: "What is Cyclops’ power in X-Men?",
+   options:["Telekinesis", "Optic blast", " Diamond skin", "all"],
+   correct:2
+  },
+  {
+   question: "What is the name of the cosmic cube that Loki takes control of in The Avengers?",
+   options:["the Tesseract", "the Magic Cube", "the Mjolnir", "all"],
+   correct:1
+  },
+  {
+   question: "How many Avengers are there in the Avengers movie?",
+   options:[" 4", " 10", "6", "100"],
+   correct:3
+  },
+  {
+   question: "How do the Fantastic Four get their powers?",
+   options:["  they were bitten by a radioactive spider", " they were exposed to cosmic radiation in space", " they were experimented on", "Dont know"],
+   correct:2
+  },
+  {
+   question: " What’s the name of Tony Stark’s company in the Iron Man trilogy?",
+   options:["  Stark and Sons", "  Stark and Co", " Stark Industries", "Avg co"],
+   correct:3
+  },
+  {
+   question:"Who does Dare Devil team up with in his film?",
+   options:[" Elektra", "  Storm", "The Invisible Woman", "Wonder Women"],
+   correct:1
+  },
+  {
+   question:"How many years was Captain America frozen for in Captain America: The First Avenger?",
+   options:[" Nearly 100 years", " Nearly 70 years", "Nearly 20 years", "Nearly 10 years"],
+   correct:2
+  },
+  {
+   question:"Who is Thor’s main romantic interest in Thor?",
+   options:["Jane Foster", "Jean Grey", "Mary Jane", "peta Jenson"],
+   correct:1
+  },
+  {
+   question:" In X2, who do the X-Men find hidden in a church?",
+   options:["The Nightcrawler", "The Silver Surfer", "The beast", "Monkey Man"],
+   correct:3
+  },
+  {
+   question:" What’s the name of Jean Grey’s alter-ego in X-Men?",
+   options:[" The Black Widow", "The Invisible Woman", "The Phoenix", "The Hulk"],
+   correct:2
+  },
+  {
+   question:"What is the name of Rhodey’s iron suit in Iron Man 3",
+   options:["Death Proof", "  War Machine", " Killing Machine", "Jump Suit"],
+   correct:1
+  }
+  ];
